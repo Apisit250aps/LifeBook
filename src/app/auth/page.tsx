dc573681dd256abb85f5e3ab00a30c8bb5052fda
YUapisit <88958116+Apisit250aps@users.noreply.github.com>
2025-01-02 12:13:53 +0700

    auth -- google authentication

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,4 +1,8 @@
-export default function Home() {
+"use client"
+
+import { signIn } from "next-auth/react"
+
+export default function Page() {
   return (
     <div className="hero bg-base-200 min-h-screen">
       <div className="hero-content text-center">
@@ -9,7 +13,12 @@ export default function Home() {
             excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
             a id nisi.
           </p>
-          <button className="btn btn-primary">Get Started</button>
+          <button
+            onClick={() => signIn("google", { redirect: false })}
+            className="btn btn-primary"
+          >
+            Continue with google
+          </button>
         </div>
       </div>
     </div>
